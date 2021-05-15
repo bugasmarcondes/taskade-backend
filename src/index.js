4a226e0985e7517bba9825b8556a5ee934537091
Bruno Marcondes <bugasmarcondes@gmail.com>
2021-05-14 23:10:00 -0300

    Connecting to the DB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const { ApolloServer, gql } = require('apollo-server')
-const dotenv = require('dotenv')
+const { ApolloServer, gql } = require('apollo-server');
+const dotenv = require('dotenv');
+const { MongoClient } = require('mongodb');
 
-dotenv.config()
+dotenv.config();
 
 const books = [
   {
@@ -12,7 +13,7 @@ const books = [
     title: 'City of Glass',
     author: 'Paul Auster',
   },
-]
+];
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -32,21 +33,42 @@ const typeDefs = gql`
   type Query {
     books: [Book]
   }
-`
+`;
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    books: (root, data, context) => {
+      console.log(context);
+      return books;
+    },
   },
-}
+};
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers })
+const { DB_URI, DB_NAME } = process.env;
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
+const start = async () => {
+  const client = new MongoClient(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  // send the context in Apollo Server below, on every Query and Mutations
+  const context = {
+    db,
+  };
+
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+};
+
+start();
